de039167732d02c7928ca44d123caf9b9c812bd3
Animesh rastogi <animesh.rastogi54@gmail.com>
2023-12-06 13:05:03 +0530

    Added Comments

--- main.go
+++ main.go
@@ -21,9 +21,17 @@ func getClientset() *kubernetes.Clientset {
 	home := homedir.HomeDir()
 	kubeconfig := flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "Location to the Kubeconfig file")
 
+	/* Check whether code is running internally or externally and authenticate accordingly
+	if kubeconfig exists {
+		build config from kubeconfig
+	} else {
+		Build Config from SA credentials
+	}
+	*/
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		log.Printf("Error Building config from Kubeconfig: %s\n", err.Error())
+
 		config, err = rest.InClusterConfig()
 		if err != nil {
 			log.Fatalf("Error Building Config: %s\n", err.Error())
